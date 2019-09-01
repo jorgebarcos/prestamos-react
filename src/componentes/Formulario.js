db8ef59925955bc628a181c67eae0960e4ec5b58
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-01 19:14:56 -0300

    Creando el Formulario

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -3,7 +3,27 @@ import React, { Component } from 'react';
 class Formulario extends Component {
 	state = {};
 	render() {
-		return <p>Hola desde Formulario</p>;
+		return (
+			<form>
+				<div>
+					<label>Cantidad Prestamo:</label>
+					<input type="number" name="cantidad" className="u-full-width" placeholder="Ejemplo: 3000" />
+				</div>
+				<div>
+					<label>Plazo para pagar</label>
+					<select name="plazo" className="u-full-width">
+						<option value="">Seleccionar</option>
+						<option value="3">3 Meses</option>
+						<option value="">6 Meses</option>
+						<option value="">12 Meses</option>
+						<option value="">24 Meses</option>
+					</select>
+				</div>
+				<div>
+					<input type="submit" value="Calcular" className="u-full-width button-primary" />
+				</div>
+			</form>
+		);
 	}
 }
 
