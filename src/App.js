3bb18dee11bca8958adab79fe9aaed979ba14fd4
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-01 21:27:11 -0300

    Introduciendo Helper Functions

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import './normalize.css';
 import './skeleton.css';
 
 import Formulario from './componentes/Formulario';
+import { calcularTotal } from './helpers';
 
 class App extends Component {
-	datosPrestamo = (cantidad, plazo) => {};
+	datosPrestamo = (cantidad, plazo) => {
+		calcularTotal();
+	};
 
 	render() {
 		return (
---
+++ src/helpers.js
@@ -0,0 +1,12 @@
+export function calcularTotal(cantidad, plazo) {
+	// Cantidades
+	// 0 a 1000 = 25%
+	// 1001 a 5000 = 20%
+	// 5001 a 10000 = 15%
+	// +10001 = 10%
+	// Meses
+	// 3 = 5%
+	// 6 = 10%
+	// 12 = 15%
+	// 24 = 20%
+}
