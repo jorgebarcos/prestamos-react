c5d74e8be0eb7aafb190e35bce57fffd50ea6399
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-01 21:57:08 -0300

    Creando un Componente para el Resultado

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ import './skeleton.css';
 
 import Formulario from './componentes/Formulario';
 import { calcularTotal } from './helpers';
+import Resultado from './componentes/Resultado';
 
 class App extends Component {
+	state = {
+		total: '',
+		cantidad: '',
+		plazo: ''
+	};
+
 	datosPrestamo = (cantidad, plazo) => {
 		const total = calcularTotal(cantidad, plazo);
+
+		// Colocar el resultado en el state junto a la cantidad y el plazo
+		this.setState({
+			total,
+			cantidad,
+			plazo
+		});
 	};
 
 	render() {
@@ -17,6 +31,8 @@ class App extends Component {
 				<h1>Cotizador de Prestamos</h1>
 				<div className="container">
 					<Formulario datosPrestamo={this.datosPrestamo} />
+
+					<Resultado />
 				</div>
 			</Fragment>
 		);
---
+++ src/componentes/Resultado.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Resultado = () => {
+	return (
+		<div className="u-full-width resultado">
+			<h2>Resultados</h2>
+		</div>
+	);
+};
+
+export default Resultado;
