2f613e41ebdb4f4bc07876fb2aaad858036a66cc
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-02 06:20:41 -0300

    Cargar un Componente de forma condicional

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './skeleton.css';
 import Formulario from './componentes/Formulario';
 import { calcularTotal } from './helpers';
 import Resultado from './componentes/Resultado';
+import Mensaje from './componentes/Mensaje';
 
 class App extends Component {
 	state = {
@@ -27,15 +28,22 @@ class App extends Component {
 
 	render() {
 		const { total, plazo, cantidad } = this.state;
+
+		// cargar un componente condicionalmente
+		let componente;
+		if (total === '') {
+			componente = <Mensaje />;
+		} else {
+			componente = <Resultado total={total} plazo={plazo} cantidad={cantidad} />;
+		}
 		return (
 			<Fragment>
 				<h1>Cotizador de Prestamos</h1>
 				<div className="container">
 					<Formulario datosPrestamo={this.datosPrestamo} />
 
-					<div className="mensajes">
-						<Resultado total={total} plazo={plazo} cantidad={cantidad} />
-					</div>
+					<div className="mensajes" />
+					{componente}
 				</div>
 			</Fragment>
 		);
