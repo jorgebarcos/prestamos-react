2aa9b0f35c1844cf070213cafa88a8591388ae96
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-02 06:36:38 -0300

    Mostrando un Spinner de Carga

--- src/App.js
+++ src/App.js
@@ -7,32 +7,46 @@ import Formulario from './componentes/Formulario';
 import { calcularTotal } from './helpers';
 import Resultado from './componentes/Resultado';
 import Mensaje from './componentes/Mensaje';
+import Spinner from './componentes/Spinner';
 
 class App extends Component {
 	state = {
 		total: '',
 		cantidad: '',
-		plazo: ''
+		plazo: '',
+		cargando: false
 	};
 
 	datosPrestamo = (cantidad, plazo) => {
 		const total = calcularTotal(cantidad, plazo);
 
 		// Colocar el resultado en el state junto a la cantidad y el plazo
-		this.setState({
-			total,
-			cantidad,
-			plazo
-		});
+		this.setState(
+			{
+				cargando: true
+			},
+			() => {
+				setTimeout(() => {
+					this.setState({
+						total,
+						cantidad,
+						plazo,
+						cargando: false
+					});
+				}, 3000);
+			}
+		);
 	};
 
 	render() {
-		const { total, plazo, cantidad } = this.state;
+		const { total, plazo, cantidad, cargando } = this.state;
 
 		// cargar un componente condicionalmente
 		let componente;
-		if (total === '') {
+		if (total === '' && !cargando) {
 			componente = <Mensaje />;
+		} else if (cargando) {
+			componente = <Spinner />;
 		} else {
 			componente = <Resultado total={total} plazo={plazo} cantidad={cantidad} />;
 		}
--- src/componentes/Resultado.js
+++ src/componentes/Resultado.js
@@ -7,7 +7,7 @@ const Resultado = (props) => {
 			<p>La cantidad Solicitada fue: $ {props.cantidad}</p>
 			<p>A pagar en: {props.plazo} Meses</p>
 			<p>Total a pagar: $ {props.total} Meses</p>
-			<p>Su pago mensual es de: $ {(props.total / props.plazo).toFixed(2)} Meses</p>
+			<p>Su pago mensual es de: $ {(props.total / props.plazo).toFixed(2)}</p>
 		</div>
 	);
 };
